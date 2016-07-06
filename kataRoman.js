1d96dba69c208f59968990aa64d3c35a9276f244
Sam Sataponn <sam@abctech-thailand.com>
2016-07-06 12:26:43 +0700

    Implement function arabicToRoman()
    - Handle only numbers contained in the mapping.
    - No commas in number strings

--- kataRoman.js
+++ kataRoman.js
@@ -1,11 +1,27 @@
 'use strict';
 
+const mapping = {
+    1: 'I',
+    5: 'V',
+    10: 'X',
+    50: 'L',
+    100: 'C',
+    500: 'D',
+    1000: 'M',
+};
+
 module.exports = {
     // string arabic (may include commas)
-    arabicToRoman: function (arabic) {
+    arabicToRoman: function(arabic) {
         let converted = '';
 
-        // TODO
+        for (let key in mapping) {
+            if (mapping.hasOwnProperty(key)) {
+                if (arabic === key) {
+                    return mapping[key];
+                }
+            }
+        }
 
         return converted;
     },
