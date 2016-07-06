de113efbea9f25f1065e722902d13f2b3ea1eee6
Sam Sataponn <sam@abctech-thailand.com>
2016-07-06 12:24:27 +0700

    Rearrange cases in the test code

--- test/test.js
+++ test/test.js
@@ -3,11 +3,21 @@
 const assert = require('chai').assert;
 const kataRoman = require('../kataRoman');
 
-describe('Convert Arabic to Roman', function() {
-    describe('arabicToRoman()', function() {
-        it('The converted Roman numbers should be correct.', function() {
-            assert.equal(kataRoman.arabicToRoman('1'), 'I');
-            assert.equal(kataRoman.arabicToRoman('1,000'), 'M');
+describe('Convert numbers that contain only 0, 1, and 5', function() {
+    describe('Convert numbers that are contained in the mapping (no commas)', function() {
+        describe('arabicToRoman()', function() {
+            it('The converted Roman numbers should be correct', function() {
+                assert.equal(kataRoman.arabicToRoman('1'), 'I');
+                assert.equal(kataRoman.arabicToRoman('50'), 'L');
+            });
+        });
+    });
+
+    describe('Convert numbers with a comma', function() {
+        describe('arabicToRoman()', function() {
+            it('Conversion should be correct', function() {
+                assert.equal(kataRoman.arabicToRoman('1,000'), 'M');
+            });
         });
     });
 });
