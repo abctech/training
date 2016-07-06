7a34669cbeb3d03656edeb2ecc921304890e535c
Sam Sataponn <sam@abctech-thailand.com>
2016-07-06 13:42:33 +0700

    Rearrange cases and add more cases

--- test/test.js
+++ test/test.js
@@ -3,21 +3,23 @@
 const assert = require('chai').assert;
 const kataRoman = require('../kataRoman');
 
-describe('Convert numbers that contain only 0, 1, and 5', function() {
-    describe('Convert numbers that are contained in the mapping (no commas)', function() {
-        describe('arabicToRoman()', function() {
-            it('The converted Roman numbers should be correct', function() {
-                assert.equal(kataRoman.arabicToRoman('1'), 'I');
-                assert.equal(kataRoman.arabicToRoman('50'), 'L');
-            });
+describe('Convert Arabic to Roman', function() {
+    describe('Convert numbers that are contained in the mapping', function() {
+        it('No commas', function() {
+            assert.equal(kataRoman.arabicToRoman('1'), 'I');
+            assert.equal(kataRoman.arabicToRoman('50'), 'L');
+        });
+
+        it('With commas', function() {
+            assert.equal(kataRoman.arabicToRoman('1,000'), 'M');
         });
     });
 
-    describe('Convert numbers with a comma', function() {
-        describe('arabicToRoman()', function() {
-            it('Conversion should be correct', function() {
-                assert.equal(kataRoman.arabicToRoman('1,000'), 'M');
-            });
-        });
+    it('Convert numbers that contain only 0, 1, and 5 (with commas)', function() {
+        assert.equal(kataRoman.arabicToRoman('15'), 'XV');
+    });
+
+    it('Convert any numbers (with commas)', function() {
+        assert.equal(kataRoman.arabicToRoman('4'), 'IV');
     });
 });
