44f79dba9de3ad1f91e7dfd6e5155f034743f8a8
Sam Sataponn <sam@abctech-thailand.com>
2016-07-06 14:04:27 +0700

    Improve function arabicToRoman()
    - Now it can handle numbers containing only 0, 1, and 5.

--- kataRoman.js
+++ kataRoman.js
@@ -12,22 +12,41 @@ const mapping = {
 
 module.exports = {
     // string number
-    removeComma: function(number) {
+    removeComma: function (number) {
         return number.replace(',', '');
     },
 
-    // string arabic (may include commas)
-    arabicToRoman: function(arabic) {
-        arabic = this.removeComma(arabic);
-        let converted = '';
+    // string number
+    extractDigit: function (number, position) {
+        let digit = number.charAt(position - 1);
+        let numZeroes = number.length - position;
 
+        digit += String(Math.pow(10, numZeroes)).substring(1);
+
+        return digit;
+    },
+
+    // string digit
+    arabicDigitToRoman: function (digit) {
         for (let key in mapping) {
             if (mapping.hasOwnProperty(key)) {
-                if (arabic === key) {
+                if (digit === key) {
                     return mapping[key];
                 }
             }
         }
+    },
+
+    // string arabic (may include a comma)
+    arabicToRoman: function (arabic) {
+        arabic = this.removeComma(arabic);
+        let converted = '';
+
+        arabic.split('').forEach((digit, index) => {
+            if (digit !== '0') {
+                converted += this.arabicDigitToRoman(this.extractDigit(arabic, index + 1));
+            }
+        });
 
         return converted;
     },
